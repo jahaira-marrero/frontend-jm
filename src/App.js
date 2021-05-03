81fa75e170df73acd8d33da843d099c8c2980f99
Jahaira <jahaira.marrero@gmail.com>
2021-05-03 12:17:17 -0400

    set up

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <p> */}
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          <h1>Welcome to jahairamarrero.com</h1>
+          <p1>WEBSITE UNDER CONSTRUCTION</p1>
+        {/* </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="jahaira@jahairamarrero.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Contact Jahaira
+        </a> */}
       </header>
     </div>
   );
